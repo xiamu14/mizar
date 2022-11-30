514b7b233d79f1f414f28af22b777b3a64e04ba9
ben <x_z_gfree@163.com>
2022-11-30 14:41:11 +0800

    fix: fix google font network error.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,14 +7,14 @@ export default function Document() {
       <Head>
         <link rel="shortcut icon" href="/assets/images/logo.svg" />
       </Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.googlefonts.cn" />
       <link
         rel="preconnect"
         href="https://fonts.gstatic.com"
         crossOrigin="crossOrigin"
       />
       <link
-        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+        href="https://fonts.googlefonts.cn/css2?family=Nunito:wght@400;600;700&display=swap"
         rel="stylesheet"
       />
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Footer from '../components/footer';
 import Navbar from '../components/navbar';
 import ContentWidth from '../layouts/content_width';
@@ -6,6 +7,9 @@ import style from '../styles/index.module.scss';
 export default function Index() {
   return (
     <div className={style.index}>
+      <Head>
+        <title>言之有物</title>
+      </Head>
       <div className={style.header}>
         <Navbar />
       </div>
